556a51b1d18a7d585b3628473b3101852121e9ac
DJ On Top <107875648+djontop@users.noreply.github.com>
2023-11-27 04:12:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-(function(_0x4c9937,_0x449565){var _0x5ca079=_0x97e4,_0x3b5d73=_0x4c9937();while(!![]){try{var _0x20a5eb=-parseInt(_0x5ca079(0x105))/0x1+-parseInt(_0x5ca079(0x103))/0x2+-parseInt(_0x5ca079(0xff))/0x3+-parseInt(_0x5ca079(0x100))/0x4+parseInt(_0x5ca079(0x104))/0x5*(parseInt(_0x5ca079(0x106))/0x6)+-parseInt(_0x5ca079(0xfa))/0x7+parseInt(_0x5ca079(0x101))/0x8;if(_0x20a5eb===_0x449565)break;else _0x3b5d73['push'](_0x3b5d73['shift']());}catch(_0x3a4f9e){_0x3b5d73['push'](_0x3b5d73['shift']());}}}(_0x588a,0x4e3b4));function _0x97e4(_0x1a66b3,_0x1422f9){var _0x588a77=_0x588a();return _0x97e4=function(_0x97e4a1,_0x30af18){_0x97e4a1=_0x97e4a1-0xf9;var _0x5be601=_0x588a77[_0x97e4a1];return _0x5be601;},_0x97e4(_0x1a66b3,_0x1422f9);}function _0x588a(){var _0x583fd3=['20764MtWmuI','15CxmSts','557582EgltPc','437238Jwtuxy','value','2899421vpqSMI','qrData','innerHTML','trim','getElementById','868173SRJzqX','1877840GvOWrb','14742680PShMRH','qrCodeDisplay'];_0x588a=function(){return _0x583fd3;};return _0x588a();}function generateQRCode(){var _0x2c9e6a=_0x97e4,_0x3c5571=document[_0x2c9e6a(0xfe)](_0x2c9e6a(0xfb))[_0x2c9e6a(0xf9)];if(_0x3c5571[_0x2c9e6a(0xfd)]()!==''){var _0x2f639d=document['getElementById'](_0x2c9e6a(0x102));_0x2f639d[_0x2c9e6a(0xfc)]='';var _0x5a769e=new QRCode(_0x2f639d,{'text':_0x3c5571,'width':0x80,'height':0x80});}}
+function generateQRCode() {
+    var qrData = document.getElementById('qrData').value;
+
+    if (qrData.trim() !== '') {
+        var qrCodeDisplay = document.getElementById('qrCodeDisplay');
+        qrCodeDisplay.innerHTML = ''; // Clear previous QR code
+
+        var qr = new QRCode(qrCodeDisplay, {
+            text: qrData,
+            width: 128,
+            height: 128
+        });
+    }
+}
